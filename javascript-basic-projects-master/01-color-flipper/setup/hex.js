c6e22cd77083f7f79fba8270dde716c7eb4e3ad5
Rio <naziincolour123@gmail.com>
2023-07-07 21:25:27 +0700

    review part 1 - part 2

--- javascript-basic-projects-master/01-color-flipper/setup/hex.js
+++ javascript-basic-projects-master/01-color-flipper/setup/hex.js
@@ -1,21 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById('btn');
+const showTextColour = document.getElementById('color');
+const getHTMLBody = document.querySelector('body');
+const len = hex.length-1;
+const hexSpace =6;
 
-const showTextColor = document.getElementById('color');
-const buttonTrigger = document.getElementById('btn');
-const getHTMLBody = document.querySelector('body')
-const len = hex.length - 1;
-
-buttonTrigger.addEventListener('click', () => {
-    const index  = '#';
-    for(let i=0; i<6; i++){
-        index+= hex[getRandomNumber()];
+btn.addEventListener('click', ()=>{
+    let newHexColour ='#';
+    for(let i =0; i<hexSpace; i++){
+        newHexColour+= hex[getRandomNumber()];
     }
-    showTextColor.innerHTML = index;
-    getHTMLBody.style.backgroundColor = index;
+    getHTMLBody.style.backgroundColor = newHexColour;
 
+    showTextColour.textContent = newHexColour;
 })
 
-
-const getRandomNumber=()=>{
-    return Math.floor(Math.random()* len);
+const getRandomNumber =()=>{
+    return Math.floor(Math.random() * len);
 }
--- javascript-basic-projects-master/02-counter/setup/review.js
+++ javascript-basic-projects-master/02-counter/setup/review.js
@@ -1,57 +1,43 @@
 let counter = 0;
-const showTextValue = document.getElementById('value');
 const btns = document.querySelectorAll('.btn');
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const showtextValue = document.getElementById('value');
+const len = hex.length - 1;
+const getHTMLBody = document.querySelector('body');
+
 
 btns.forEach((btn) => {
-    console.log(btn);
+    console.log(`The value of btn ${btn}`);
     btn.addEventListener('click', (e) => {
-        console.log(e.currentTarget.value);
-        const style = e.currentTarget.classList;
+        let style = e.currentTarget.classList;
+        console.log(`The value of style var : ${style}`)
 
-        style.contains('decrease') ?counter--
-        :style.contains('increase')? counter++
-        :style.contains('reset') ? counter = 0
-        : style.contain('multiply') ? counter *=2
-        : style.contains('divide') ? counter /=2 :0
+        style.contains('decrease') ? counter-- : 0
+        style.contains('increase') ? counter++ : 0
+        style.contains('reset') ? counter = 0 : 0
+        style.contains('multiply') ? counter *= 2 : 0
+        style.contains('divide') ? counter /= 2 : 0
 
-        counter>0 ? showTextValue.style.color ="green" : 0
-        counter<0 ? showTextValue.style.color = "red" : 0
-        counter === 0 ? showTextValue.style.color = "skyblue" : 0
+        if (style.contains('colour')) {
+            let newHexColour = '#'
+            for (let i = 0; i < 6; i++) {
+                newHexColour += hex[getRandomNumber()]
+            }
 
-        // if (style.contains('decrease')) {
-        //     counter--;
-        // }
-        // else if (style.contains('increase')) {
-        //     counter++;
-        // }
-        // else if (style.contains('reset')) {
-        //     counter = 0;
-        // }
-        // else if (style.contains('multiply')) {
-        //     counter *=2;
-        // }
-        // else if(style.contains('divide')){
-        //     counter/=2;
-        // }
-        // else if(style.contains('exponent')){
-        //     counter %= 2;
-        // }
-        // else {
-        //     return 0;
-        // // }
-        // if (counter < 0) {
-        //     showTextValue.style.color = "red"
+            getHTMLBody.style.backgroundColor = newHexColour;
+        }
 
-        // }
-        // if (counter > 0) {
-        //     showTextValue.style.color = "green"
-        // }
-        // if (counter=== 0){
-        //     showTextValue.style.color ="skyblue"
-        // }
+        counter > 0 ? showtextValue.style.color = 'greem' : 0;
+        counter < 0 ? showtextValue.style.color = 'red' : 0;
+        counter == 0 ? showtextValue.style.color = 'skyblue' : 0;
 
-        showTextValue.textContent = counter;
+
+        showtextValue.textContent = counter;
+
+
     })
 })
 
-
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * len);
+}
