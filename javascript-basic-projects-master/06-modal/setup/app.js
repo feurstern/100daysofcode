808722ab6f157e01d6a5f81a0504e38e492808d6
梨央Rio <rio-kurniawan@sakura-schutzstaffel.com>
2023-08-19 21:07:35 +0700

    finished to 06.modal challenge

--- javascript-basic-projects-master/06-modal/setup/app.js
+++ javascript-basic-projects-master/06-modal/setup/app.js
@@ -2,3 +2,18 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const btnTrigger = document.querySelector('.modal-btn');
+const mdOverlay = document.querySelector('.modal-overlay');
+const clsBtn = document.querySelector('.close-btn');
+console.log(btnTrigger, mdOverlay, clsBtn);
+
+btnTrigger.addEventListener('click', ()=>{
+    mdOverlay.classList.add('open-modal')
+    // alert('clikc')
+})
+
+clsBtn.addEventListener('click', ()=>{
+    mdOverlay.classList.remove('open-modal')
+    alert('clicked!')
+})
